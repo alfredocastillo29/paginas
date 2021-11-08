d7ecef6f4339d1c00f8bee5c2f55f0cf64654a92
Alfredo Castillo <alfredo.castillo@frogslayer.com>
2021-11-08 07:32:47 -0600

    Login con facebook

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase_config : {
+    apiKey: "AIzaSyC1zeqxSOA_PLUZgDn9BAA5Z_lrsu5lWQo",
+    authDomain: "fir-test-b3d32.firebaseapp.com",
+    databaseURL: "https://fir-test-b3d32-default-rtdb.firebaseio.com",
+    projectId: "fir-test-b3d32",
+    storageBucket: "fir-test-b3d32.appspot.com",
+    messagingSenderId: "872620998076",
+    appId: "1:872620998076:web:b2f328099ca995b41dffb9" 
+  }
 };
